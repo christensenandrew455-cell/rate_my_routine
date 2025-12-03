1f34be1759752bc58bc7b0b524cd8a90644a2b1d
christensenandrew455-cell <christensenandrew455@gmail.com>
2025-12-03 12:08:09 -0500

    Update TimeDropdown.js

--- components/TimeDropdown.js
+++ components/TimeDropdown.js
@@ -1,23 +1,31 @@
-export default function TimeDropdown({ label, value, setValue }) {
-const times = [];
-for (let h = 1; h <= 12; h++) {
-times.push(`${h}:00 AM`);
-times.push(`${h}:00 PM`);
-}
+export default function TimeDropdown({ label, hour, setHour, meridiem, setMeridiem }) {
+  return (
+    <div className="flex gap-3 items-center">
+      <div className="flex-1">
+        <label className="block mb-1 font-medium">{label}</label>
+        <div className="flex gap-2">
+          {/* Hour dropdown */}
+          <select
+            value={hour}
+            onChange={(e) => setHour(e.target.value)}
+            className="border p-2 rounded w-24"
+          >
+            {Array.from({ length: 12 }, (_, i) => i + 1).map((n) => (
+              <option key={n} value={n}>{n}</option>
+            ))}
+          </select>
 
-
-return (
-<div>
-<label className="block mb-1 font-medium">{label}</label>
-<select
-value={value}
-onChange={(e) => setValue(e.target.value)}
-className="border p-2 rounded w-full"
->
-{times.map((t) => (
-<option key={t}>{t}</option>
-))}
-</select>
-</div>
-);
+          {/* AM / PM */}
+          <select
+            value={meridiem}
+            onChange={(e) => setMeridiem(e.target.value)}
+            className="border p-2 rounded w-20"
+          >
+            <option>AM</option>
+            <option>PM</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
 }
